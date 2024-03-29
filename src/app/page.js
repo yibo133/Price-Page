ee999b5722c289e76661e0c781ed3e8e745cbc47
Yibo Zhang <zhangyb07@gmial.com>
2024-03-29 12:12:06 -0400

    price_page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,185 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import "./price-page.css";
 
 export default function Home() {
+  const [isAnnualBilling, setIsAnnualBilling] = useState(true);
+
+  const toggleBilling = () => {
+    setIsAnnualBilling(!isAnnualBilling);
+  };
+  const buyProduct1 = async (productId) => {
+    try {
+      const response = await axios.post("/api/purchaseProduct", { productId });
+      console.log(response.data);
+      window.open(response.data.checkoutUrl, "_blank");
+    } catch (error) {
+      console.error(error);
+      alert("Failed to buy product #1");
+    }
+  };
+
+  const free = [
+    "720p HD downloads",
+    "1 stock video per project",
+    "1 stock audio per project",
+    "Free AI credits",
+    "Maximum 12 projects",
+    "Video length up to 10 mins",
+  ];
+  const plusFeatures = [
+    "1080p Full-HD downloads",
+    "5 stock videos per project",
+    "5 stock audio per project",
+    "Plus AI credits",
+    "Save unlimited projects online",
+    "Unlimited video length",
+    "Custom branding",
+    "30GB cloud storage",
+    "100GB video hosting space",
+    "Upload custom fonts",
+    "Save up to 100 templates",
+    "No FlexClip watermark",
+  ];
+
+  const business = [
+    "Unlimited stock videos",
+    "4K Ultra HD Downloads",
+    "Business AI credits",
+    "Unlimited stock audio",
+    "Save unlimited projects online",
+    "Unlimited video length",
+    "Custom branding",
+    "100GB cloud storage",
+    "1TB video hosting space",
+    "Upload custom fonts",
+    "Save up to 200 templates",
+    "No FlexClip watermark",
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="group">
+      <p className="headtext">Choose the plan that's right for you</p>
+      <div className="Bill">
+        <div className="div-_text">
+          <p className="billed">Billed Monthly</p>
+        </div>
+        <label className="switch">
+          <input type="checkbox" checked={isAnnualBilling} onChange={toggleBilling} />
+          <span className="slider"></span>
+        </label>
+        <div className="div-_text">
+          <p className="billed">Billed Annually</p>
+        </div>
+        <div className="div-_cut-_tip">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/v0d0t9zjzuo-3%3A2214?alt=media&token=2e722c07-6ca3-4f36-bc5b-ced834d9973c"
+            alt="Not Found"
+            className="icon"
+          />
+          <p className="save">save up to 50%</p>
         </div>
       </div>
+      <div className="div-_list">
+        <div className="div-_listitem">
+          <p className="Text">Free</p>
+          <div className="div-_price1">
+            <p className="price">$0</p>
+            <p className="mo">/mo</p>
+          </div>
+          <div className="div-_select-_btnfree">
+            <p className="choose-free">Choose Free</p>
+          </div>
+          <div className="div-_feature-_list">
+            {free.map((feature, index) => (
+              <div key={index} className="div-_featureitem">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/v0d0t9zjzuo-3%3A2007?alt=media&token=8358bd7c-6ef9-4a43-aaa4-aca00aae5cbc"
+                  alt="Feature Icon"
+                  className={`icon-`}
+                />
+                <p className={`feature-text-`}>{feature}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="div-_listitem1">
+          <p className="Text">Plus</p>
+          <div className="div-_price1">
+            <p className="price">{isAnnualBilling ? "$9.99" : "$19.99"}</p>
+            {isAnnualBilling ? (
+              <div>
+                <div className="div-_unit">
+                  <div className="span-_cut">
+                    <p className="discont">$19.99</p>
+                  </div>
+                </div>
+                <p className="mo">/mo</p>
+              </div>
+            ) : (
+              <p className="mo">/mo</p>
+            )}
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          <button
+            className="div-_select-_btn"
+            onClick={() => buyProduct1("304756")}
+          >
+            <div className="choose-business">Choose Business</div>
+          </button>
+          <div className="div-_feature-_list">
+            {business.map((feature, index) => (
+              <div key={index} className="div-_featureitem">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/v0d0t9zjzuo-3%3A2007?alt=media&token=8358bd7c-6ef9-4a43-aaa4-aca00aae5cbc"
+                  alt="Feature Icon"
+                  className={`icon-`}
+                />
+                <p className={`feature-text-`}>{feature}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="div-_listitem">
+          <p className="Text">Business</p>
+          <div className="div-_price1">
+            <p className="price">{isAnnualBilling ? "$19.99" : "$29.99"}</p>
+            {isAnnualBilling ? (
+              <div>
+                <div className="div-_unit">
+                  <div className="span-_cut">
+                    <p className="discont">$29.99</p>
+                  </div>
+                </div>
+                <p className="mo">/mo</p>
+              </div>
+            ) : (
+              <p className="mo">/mo</p>
+            )}
+          </div>
+
+          <button
+            className="div-_select-_btn"
+            onClick={() => buyProduct1("304938")}
+          >
+            <div className="choose-business">Choose Business</div>
+          </button>
+          <div className="div-_feature-_list">
+            {business.map((feature, index) => (
+              <div key={index} className="div-_featureitem">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/v0d0t9zjzuo-3%3A2007?alt=media&token=8358bd7c-6ef9-4a43-aaa4-aca00aae5cbc"
+                  alt="Feature Icon"
+                  className={`icon-`}
+                />
+                <p className={`feature-text-`}>{feature}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
